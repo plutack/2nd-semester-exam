8bd79d16c2d3a69c972f079bb6e964f2023b10a2
Talut Salako <salakotalut@gmail.com>
2024-04-19 16:21:42 +0100

    implement joi to post route for validation check for new post creation

--- src/routes/post_route.js
+++ src/routes/post_route.js
@@ -2,15 +2,19 @@
 import { Router } from "express";
 import * as postController from "../controller/post_controller.js";
 import { authMiddleware } from "../middleware/auth_middleware.js";
-import { generateMiddleWare } from "../middleware/routeMiddleware.js";
+import { generateMiddleware } from "../middleware/routeMiddleware.js";
 import { postValidationSchema } from "../validation/postValidation.js";
 const postRoute = Router();
 
 // match route to their respective controller and add auth middleware to protected routes
-postRoute.post("/", authMiddleware, postController.createPost);
-postRoute.get("/:id", authMiddleware, postController.getSinglePost);
-postRoute.patch("/:id", authMiddleware, postController.updatePost);
-postRoute.delete("/:id", authMiddleware, postController.deletePost);
+postRoute.post(
+  "/",
+  generateMiddleware(postValidationSchema),
+  postController.createPost,
+);
+postRoute.get("/:id", postController.getSinglePost);
+postRoute.patch("/:id", postController.updatePost);
+postRoute.delete("/:id", postController.deletePost);
 postRoute.get("/", postController.getAllPosts);
 
 export default postRoute;
