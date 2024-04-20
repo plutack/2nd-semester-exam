bd1acf38133e8e98630ee6cc486d60397f172447
Talut Salako <salakotalut@gmail.com>
2024-04-20 06:42:10 +0100

    add bcrypt function to seeded admin user password

--- src/seeder/adminSeeder.js
+++ src/seeder/adminSeeder.js
@@ -1,11 +1,11 @@
 import { connect } from "../database/connection.js";
-
+import bcrypt from "bcrypt";
 export const userSeeder = async () => {
   const admin = {
     firstName: process.env.ADMIN_FIRSTNAME,
     lastName: process.env.ADMIN_LASTNAME,
     email: process.env.ADMIN_EMAIL,
-    password: process.env.ADMIN_PASSWORD,
+    password: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
     role: "ADMIN",
   };
   const mongoDB = await connect(process.env.MONGO_URL);
